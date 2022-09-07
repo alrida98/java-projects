ed764b6bffdfcc8c422a58aa9cf9dab78de5b39d
alrida98 <alridam98@gmail.com>
2022-09-07 13:07:59 +0100

    Part 2:Solution

---
+++ Restaurant_project/src/main/java/Restaurant.java
@@ -0,0 +1,81 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Restaurant {
+    public static Restaurant findItemByName;
+    private String name;
+    private String location;
+    public LocalTime openingTime;
+    public LocalTime closingTime;
+    private static List<Item> menu = new ArrayList<Item>();
+
+    public Restaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        this.name = name;
+        this.location = location;
+        this.openingTime = openingTime;
+        this.closingTime = closingTime;
+    }
+
+
+    public boolean isRestaurantOpen() {
+        //return true;
+        //DELETE ABOVE STATEMENT AND WRITE CODE HERE
+
+        LocalTime now = getCurrentTime();
+//If the Current Time is between Opening and Closing Time
+        if (now.isAfter(openingTime) && now.isBefore(closingTime)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public LocalTime getCurrentTime() {
+        return LocalTime.now();
+    }
+
+    public List<Item> getMenu() {
+        return Collections
+                .unmodifiableList(menu);
+
+        //DELETE ABOVE RETURN STATEMENT AND WRITE CODE HERE
+    }
+
+    private static Item findItemByName(String itemName) {
+        for (Item item : menu) {
+            if (item.getName().equals(itemName))
+                return item;
+        }
+        return null;
+    }
+
+    public void addToMenu(String name, int price) {
+        Item newItem = new Item(name, price);
+        menu.add(newItem);
+    }
+
+    public void removeFromMenu(String itemName) throws itemNotFoundException {
+
+        Item itemToBeRemoved = findItemByName(itemName);
+        if (itemToBeRemoved == null)
+            throw new itemNotFoundException(itemName);
+
+        menu.remove(itemToBeRemoved);
+    }
+
+    public void displayDetails() {
+        System.out.println("Restaurant:" + name + "\n"
+                + "Location:" + location + "\n"
+                + "Opening time:" + openingTime + "\n"
+                + "Closing time:" + closingTime + "\n"
+                + "Menu:" + "\n" + getMenu());
+
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
---
+++ Restaurant_project/src/main/java/RestaurantService.java
@@ -0,0 +1,51 @@
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestaurantService {
+    private static List<Restaurant> restaurants = new ArrayList<>();
+   // public static Restaurant findRestaurantByName;
+
+    public RestaurantService()
+    {
+        LocalTime openingTime = LocalTime.parse("10:30:00");
+        LocalTime closingTime = LocalTime.parse("22:00:00");
+        restaurants.add(new Restaurant("Amelie's cafe","Chennai",openingTime,closingTime));
+    }
+    public Restaurant findRestaurantByName(String restaurantName) {
+
+        for (Restaurant restaurant : restaurants) {
+            if (restaurantName != restaurant.getName()) {
+                throw new restaurantNotFoundException(restaurantName);
+            } else if ( restaurant.getName().equals(restaurantName)) {
+                return restaurant;
+            }
+        }
+
+        return null;
+
+    }
+
+
+
+
+
+
+
+    //
+    public Restaurant addRestaurant(String name, String location, LocalTime openingTime, LocalTime closingTime) {
+        Restaurant newRestaurant = new Restaurant(name, location, openingTime, closingTime);
+        restaurants.add(newRestaurant);
+        return newRestaurant;
+    }
+
+    public Restaurant removeRestaurant(String restaurantName) throws restaurantNotFoundException {
+        Restaurant restaurantToBeRemoved = findRestaurantByName(restaurantName);
+        restaurants.remove(restaurantToBeRemoved);
+        return restaurantToBeRemoved;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+}
